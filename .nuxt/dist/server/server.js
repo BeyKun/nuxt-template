ffae97d573779b3a46221849bb5c00e880532c49
BeyKun <bayurasukmaraga@gmail.com>
2020-11-09 23:21:12 +0700

    build v1.3

--- .nuxt/dist/server/server.js
+++ .nuxt/dist/server/server.js
@@ -4356,7 +4356,7 @@ const setupProgress = axios => {
   // runtimeConfig
   const runtimeConfig = ctx.$config && ctx.$config.axios || {}; // baseURL
 
-  const baseURL =  false ? undefined : runtimeConfig.baseURL || process.env._AXIOS_BASE_URL_ || 'http://simpulkendali-api.test:8080'; // Create fresh objects for all default header scopes
+  const baseURL =  false ? undefined : runtimeConfig.baseURL || process.env._AXIOS_BASE_URL_ || 'http://simpulkendali-api.ckmindramayu.com'; // Create fresh objects for all default header scopes
   // Axios creates only one which is shared across SSR requests!
   // https://github.com/mzabriskie/axios/blob/master/lib/defaults.js
 
